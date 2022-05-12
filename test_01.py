15badb1ec1b6b4411f026960dbf7ed9f3e7f5d86
Kirlin Mensah <kirlinmensah@gmail.com>
2022-05-12 13:15:41 -0700

    This is test_01

--- test_01.py
+++ test_01.py
@@ -1,5 +1,5 @@
-def addition(num1,num2):
-    num = num1 + num2
-    return num
+def addition(num1,num2):      # this initiates a function with two parameters
+    num = num1 + num2         #  this sums the wo parameters and stores it in num
+    return num                # this returns the function when called
 
-print(addition(25,10))
+print(addition(25,10))         #this prints the result when the function is called with parameters.
